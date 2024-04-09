cb04eee9eadfefe4d3f06ba0b1bb73addac587f2
Alessandro Kantousian <alessandro_kantousian@hotmail.com>
2024-04-09 00:24:29 -0300

    Ajustes e correções

--- Temp/TextWebPlugIn/Pages/Editors/Editor.cshtml.cs
+++ Temp/TextWebPlugIn/Pages/Editors/Editor.cshtml.cs
@@ -1,24 +1,47 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TextWebPlugIn.Interfaces.Service;
+using TextWebPlugIn.Interfaces.Service.Dtos;
 using TextWebPlugIn.ViewModels;
 
 namespace TextWebPlugIn.Pages.Editors;
 
 [IgnoreAntiforgeryToken]
-public class EditorModel : PageModel { 
+public class EditorModel : PageModel {
     [BindProperty]
-    public CmsViewModel? CmsViewModel { get; set; }
+    public CmsViewModel? CmsViewModel { get; set; } = new();
 
-    public void OnGet() {
+    private readonly ICmsAppService _cmsAppService;
+    private readonly IMapper _mapper;
+
+    public EditorModel(ICmsAppService cmsAppService, IMapper mapper) {
+        _cmsAppService = cmsAppService;
+        _mapper = mapper;
     }
 
-    public IActionResult OnPost() {
+    public async void OnGet() {
+        //TODO Receber Guid
+        CmsEntityDto? cmsEntityDto = (await _cmsAppService.GetAll()).FirstOrDefault();
+
+        var result = await _cmsAppService.GetCMSContent(cmsEntityDto.Id);
+
+        if (result is not null) { 
+            CmsViewModel = _mapper.Map<CmsViewModel>(result);
+        }
+    }
+
+    public async Task<IActionResult> OnPost() {
         if (!ModelState.IsValid) {
             return Page();
         }
 
-        // Aqui você pode adicionar o código para salvar cmsViewModel no banco de dados
+        if (CmsViewModel is not null) {
+            CmsEntityDto cmsEntityDto = _mapper.Map<CmsEntityDto>(CmsViewModel);
 
-        return RedirectToPage("./Index");
+            await _cmsAppService.InsertOrUpdateCMSContent(cmsEntityDto);
+        }
+
+        return RedirectToPage("./mainpage");
     }
 }
--- Temp/TextWebPlugIn/ViewModels/CmsViewModel.cs
+++ Temp/TextWebPlugIn/ViewModels/CmsViewModel.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace TextWebPlugIn.ViewModels;
 
 public class CmsViewModel {
     public Guid Id { get; set; }
 
+    [Required]
     public string? Name { get; set; }
 
+    [Required]
     public string? Description { get; set; }
 }
